9e29dcee9a5077d70141b43f7876a87448d093e1
R Richards <rdrrichards@hotmail.com>
2019-03-08 17:09:06 -0600

    ng/cli/ionic updates to latest

--- ng-update.bat
+++ ng-update.bat
@@ -1,3 +1,6 @@
 CALL ng update @angular/cli
 CALL ng update @angular/core
 CALL ng update @angular-devkit/build-angular
+CALL ng update @angular-devkit/architect
+CALL ng update @angular-devkit/core
+CALL ng update @angular-devkit/schematics
