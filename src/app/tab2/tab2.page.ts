d20112f68d780651f985ced3aee0ce5c5b67fe72
R. Richards <rdrrichards@hotmail.com>
2019-05-13 18:18:53 -0500

    updated user page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+  users: any[];
+  constructor(private userService: UserService) {
+
+  }
+  ngOnInit() {
+    this.userService.getUsers().valueChanges().subscribe(users => {
+      this.users = users;
+    });
+  }
+  clickUser(email: string) {
+    console.log(email);
+  }
+}
