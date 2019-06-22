b4ea9c2163067414d1a8a66f631a7791134a4ce4
R. Richards <rdrrichards@hotmail.com>
2019-06-22 10:37:22 -0500

    pre update commit

--- ng-update.bat
+++ ng-update.bat
@@ -1,6 +1,8 @@
+CALL git commit -am "pre update commit"
 CALL ng update @angular/cli
+CALL git commit -am "cli update to latest"
 CALL ng update @angular/core
-CALL ng update @angular-devkit/build-angular
-CALL ng update @angular-devkit/architect
-CALL ng update @angular-devkit/core
-CALL ng update @angular-devkit/schematics
+CALL git commit -am "ng update to latest"
+CALL npm audit fix
+CALL git commit -am "post update commit"
+CALL build-test.bat
