0d49e69755a7b86ef521137e383289d66b1e1b85
R. Richards <rdrrichards@hotmail.com>
2020-07-11 13:23:37 -0500

    pre update commit

--- ng-upd.bat
+++ ng-upd.bat
@@ -1,7 +1,9 @@
 CALL git commit -am "pre update commit"
-CALL ng update @angular/core
+CALL ng update @angular/core --force
+CALL git add .
 CALL git commit -am "@angular/core update to latest"
-CALL ng update @angular/cli
+CALL ng update @angular/cli --force
+CALL git add .
 CALL git commit -am "@angular/cli update to latest"
 CALL ng update @angular-devkit/architect
 CALL git commit -am "@angular-devkit/architect to latest"
